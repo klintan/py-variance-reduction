054f560cc99991b091aaace414bf18269fa43129
Andreas Klintberg <ankl@kth.se>
2016-04-18 22:35:00 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Variance reduction for Decision Trees Regression split evaluation
+
+This function computes the relative variance reduction score associated with each split on the targets values. Similar to what is done in sklearn and DecisionTreeRegressor (mse criterion):
+
+It expects the input to be panda dataframes (y, yleft and yright). Returns score as a float value.
+
+Inputs :
+y         = full pandas dataset y values
+yleft     = split left group y dataframe
+yright     = split right group y dataframe
+
+Outputs :
+scores    = relative variance reduction associated with each split
+
+
+This module and function has been derived from https://github.com/rtaormina/MATLAB_ExtraTrees/blob/master/Functions/Regression/computeScores_r.m
+
+For any improvements of this module feel free to do a pull request.
---
+++ variance_reduction.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+
+class VarianceReduction():
+    """
+     This function computes the relative variance reduction score associated
+    with each split on the targets values. Similar to what is done in sklearn and DecisionTreeRegressor (mse criterion):
+
+    Inputs :
+    y         = full pandas dataset y values
+    yleft     = split left group y dataframe
+    yright     = split right group y dataframe
+
+
+    Outputs :
+    scores    = relative variance reduction associated with each split
+
+
+    This module and function has been derived from https://github.com/rtaormina/MATLAB_ExtraTrees/blob/master/Functions/Regression/computeScores_r.m
+
+    Attributes:
+        score: The computed score
+    """
+
+    def __init__(self):
+        """Initialize variance reduction scoring
+        """
+
+    def compute_score(self, y, yleft, yright, split):
+        """Computes variance score for split."""
+
+        # get dataset length n and number of attributes nAtt
+        n = len(y.index)
+
+        # compute target variance for the dataset
+        y_var = y.var()
+
+        # compute target variance for the two branches
+        yleft_var = yleft.var()
+        yright_var = yright.var()
+
+        nleft = len(yleft.index)
+        nright = len(yright.index)
+
+        # compute scores
+        scores = (y_var - nleft/n.*yleft_var - nright/n.*yright_var)/y_var;
+        return scores
