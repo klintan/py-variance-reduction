30d3dff0ef7e9ae417f0be684d2741c12a14cc85
Andreas Klintberg <ankl@kth.se>
2016-04-21 22:42:16 +0200

    added init.py and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='VarianceReduction',
+    version='0.1dev',
+    packages=['variancereduction',],
+    license='GNUv3',
+    long_description=open('Readme.md').read(),
+)
